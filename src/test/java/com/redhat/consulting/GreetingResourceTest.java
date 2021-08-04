3b181abae107c774407f6a5815c40856aa82f12a
Roland T. Lichti <rlichti@redhat.com>
2021-08-04 13:29:29 +0200

    Initial commit

---
+++ src/test/java/com/redhat/consulting/GreetingResourceTest.java
@@ -0,0 +1,42 @@
+package com.redhat.consulting;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when()
+                .get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+    @Test
+    public void testAuthHelloEndpoint() {
+        given()
+                .when()
+                .auth().basic("test","test")
+                .get("/hello/auth")
+                .then()
+                .statusCode(200)
+                .body(is("Hello RESTEasy"));
+    }
+
+
+    @Test
+    public void testAuthFailingHelloEndpoint() {
+        given()
+                .when()
+                .get("/hello/auth")
+                .then()
+                .statusCode(403);
+    }
+}
